e744b46ad7b657baca5e91115fedc75d94964a7d
Đinh Văn Thuận - CMC Global DU1 <dvthuan@LDAP.CMCGLOBAL.COM.VN>
2019-12-06 15:12:02 +0700

    branch dev-dvthuan

---
+++ src/layouts/ant-theme/ant-layout.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import ProLayout from "@ant-design/pro-layout";
+import BasicLayout, {
+   PageHeaderWrapper,
+   RightContent,
+   DefaultFooter
+} from "@ant-design/pro-layout";
+import { Icon, Result, Button } from "antd";
+// import UserList from "../components/users/userList";
+const defaultFooterDom = (
+   <DefaultFooter
+      copyright="2019 蚂蚁金服体验技术部出品"
+      links={[
+         {
+            key: "Ant Design Pro",
+            title: "Ant Design Pro 123",
+            href: "https://pro.ant.design",
+            blankTarget: true
+         },
+         {
+            key: "github",
+            title: <Icon type="github" />,
+            href: "https://github.com/ant-design/ant-design-pro",
+            blankTarget: true
+         },
+         {
+            key: "Ant Design",
+            title: "Ant Design",
+            href: "https://ant.design",
+            blankTarget: true
+         }
+      ]}
+   />
+);
+
+const footerRender = () => {
+   return (
+      <>
+         {defaultFooterDom}
+         <div>
+            <a
+               href="https://www.netlify.com"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               <img
+                  src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg"
+                  width="82px"
+                  alt="netlify logo"
+               />
+            </a>
+         </div>
+      </>
+   );
+};
+
+class AntLayout extends React.Component {
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      return (
+         <ProLayout
+            menuDataRender={() => [
+               {
+                  path: "/",
+                  name: "WELCOME",
+                  icon: "smile",
+                  children: [
+                     {
+                        path: "/welcome",
+                        name: "ONE",
+                        children: [
+                           {
+                              path: "/welcome/welcome",
+                              name: "ONE 1",
+                              icon: "smile",
+                              exact: true,
+                              key: 1
+                           },
+                           {
+                              path: "/welcome/welcome",
+                              name: "ONE 2",
+                              icon: "smile",
+                              exact: true,
+                              key: 2
+                           }
+                        ]
+                     }
+                  ]
+               }
+            ]}
+            locale={true}
+            fixedHeader={true}
+            // {...defaultProps}
+            fixSiderbar={true}
+            title="Remax"
+            logo="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*1NHAQYduQiQAAAAAAAAAAABkARQnAQ"
+            menuHeaderRender={(logo, title) => (
+               <div
+                  // Id="customize_menu_header"
+                  onClick={() => {
+                     Window.open("https://remaxjs.org/");
+                  }}
+               >
+                  {logo}
+                  {title}
+               </div>
+            )}
+            // rightContentRender={}
+            footerRender={footerRender}
+         >
+            <PageHeaderWrapper content="Welcome to your use">
+               {this.props.children}
+            </PageHeaderWrapper>
+
+            {/* <UserList>{children}</UserList> */}
+         </ProLayout>
+      );
+   }
+}
+
+export default AntLayout;
--- src/routers/routerConfig.js
+++ src/routers/routerConfig.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Route, Router, Switch } from "react-router-dom";
-import Index from '../views/index';
-import Page01 from '../views/page01';
-import Page02 from '../views/page02';
-import Page03 from '../views/page03';
+import Index from "../views/index";
+import Page01 from "../views/page01";
+import Page02 from "../views/page02";
+import Page03 from "../views/page03";
+import AntLayout from "../layouts/ant-theme/ant-layout";
 function RouterConfig({ history }) {
-  return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={Index} />
-        <Route path="/page01" exact component={Page01} />
-        <Route path="/page02" exact component={Page02} />
-        <Route path="/page03" exact component={Page03} />
-      </Switch>
-    </Router>
-  );
+   return (
+      <Router history={history}>
+         <Switch>
+            <Route path="/login" exact component={AntLayout} />
+            <Route path="/" exact component={AntLayout} />
+            <Route path="/page01" exact component={Page01} />
+            <Route path="/page02" exact component={Page02} />
+            <Route path="/page03" exact component={Page03} />
+         </Switch>
+      </Router>
+   );
 }
 export default RouterConfig;
