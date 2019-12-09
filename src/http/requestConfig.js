b166e9d281448c2117d7a4ff7f8d7185df344cb1
dtdat2 <dtdatcmc@gmail.com>
2019-12-09 15:14:41 +0700

    re-structure project

---
+++ src/http/requestConfig.js
@@ -0,0 +1,11 @@
+export const requestCode = {
+    success: '00000',
+    expired: '401',
+    forceExpired: '400',
+    alreadyExits: '90007'
+
+}
+export const requestStatus = {
+    expired: 401,
+    forceExpired: 400
+}
