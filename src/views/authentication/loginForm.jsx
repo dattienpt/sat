3bb0d73dc0d5ce3a7f6ff543be6a02320844275b
Đinh Văn Thuận - CMC Global DU1 <dvthuan@LDAP.CMCGLOBAL.COM.VN>
2019-12-09 15:56:27 +0700

    authentication-login

--- src/views/authentication/loginForm.jsx
+++ src/views/authentication/loginForm.jsx
@@ -20,70 +20,73 @@ class NormalLoginForm extends React.Component {
                type="flex"
                justify="center"
                align="middle"
-               style={{ minHeight: "100vh" }}
+               style={{ minHeight: "80vh",  }}
             >
-               <Col>
-                  <Form onSubmit={this.handleSubmit}>
-                     <Form.Item>
-                        {getFieldDecorator("username", {
-                           rules: [
-                              {
-                                 required: true,
-                                 message: "Please input your username!"
-                              }
-                           ]
-                        })(
-                           <Input
-                              prefix={
-                                 <Icon
-                                    type="user"
-                                    style={{ color: "rgba(0,0,0,.25)" }}
-                                 />
-                              }
-                              placeholder="Username"
-                           />
-                        )}
-                     </Form.Item>
-                     <Form.Item>
-                        {getFieldDecorator("password", {
-                           rules: [
-                              {
-                                 required: true,
-                                 message: "Please input your Password!"
-                              }
-                           ]
-                        })(
-                           <Input
-                              prefix={
-                                 <Icon
-                                    type="lock"
-                                    style={{ color: "rgba(0,0,0,.25)" }}
-                                 />
-                              }
-                              type="password"
-                              placeholder="Password"
-                           />
-                        )}
-                     </Form.Item>
-                     <Form.Item>
-                        {getFieldDecorator("remember", {
-                           valuePropName: "checked",
-                           initialValue: true
-                        })(<Checkbox>Remember me</Checkbox>)}
-                        <a className="login-form-forgot" href="">
-                           Forgot password
-                        </a>
-                     </Form.Item>
-                     <Form.Item>
-                        <Button
-                           type="primary"
-                           htmlType="submit"
-                           className="login-form-button"
-                        >
-                           Log in
-                        </Button>
-                     </Form.Item>
-                  </Form>
+               <Col >
+                  <div className="form-login-box">
+                     <Form
+                        onSubmit={this.handleSubmit}
+
+                     >
+                        <Form.Item>
+                           {getFieldDecorator("username", {
+                              rules: [
+                                 {
+                                    required: true,
+                                    message: "Please input your username!"
+                                 }
+                              ]
+                           })(
+                              <Input
+                                 prefix={
+                                    <Icon
+                                       type="user"
+                                       style={{ color: "rgba(0,0,0,.25)" }}
+                                    />
+                                 }
+                                 placeholder="Username"
+                                 className="ant-input-lg"
+                              />
+                           )}
+                        </Form.Item>
+                        <Form.Item>
+                           {getFieldDecorator("password", {
+                              rules: [
+                                 {
+                                    required: true,
+                                    message: "Please input your Password!"
+                                 }
+                              ]
+                           })(
+                              <Input
+                                 prefix={
+                                    <Icon
+                                       type="lock"
+                                       style={{ color: "rgba(0,0,0,.25)" }}
+                                    />
+                                 }
+                                 type="password"
+                                 placeholder="Password"
+                                 className="ant-input-lg"
+                              />
+                           )}
+                        </Form.Item>
+                        <Form.Item>
+                           <a className="login-form-forgot" href="">
+                              Forgot password
+                           </a>
+                        </Form.Item>
+                        <Form.Item>
+                           <Button
+                              type="primary"
+                              htmlType="submit"
+                              className="login-form-button"
+                           >
+                              Log in
+                           </Button>
+                        </Form.Item>
+                     </Form>
+                  </div>
                </Col>
             </Row>
          </div>
