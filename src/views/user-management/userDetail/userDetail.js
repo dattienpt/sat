4386a59d858120a3dcd6e1dc4298a891cdc7047f
CungThien <minhvuongbvh@gmail.com>
2019-12-12 17:56:12 +0700

    update ui

--- src/views/user-management/userDetail/userDetail.js
+++ src/views/user-management/userDetail/userDetail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import style from  './userDetail.scss';
-import { Button, Modal,  } from 'antd';
+import { Button, Modal,Icon  } from 'antd';
 
 class UserDetail extends Component {
     state = { visible:false,   }
@@ -46,14 +46,14 @@ class UserDetail extends Component {
             <div className= {style.container}>
             
               <div className={style.box_left}> 
-    <div >Full name: </div> 
+    <div ><Icon type="user" /> Full name: </div> 
     <div>{this.props.user.firstname} {this.props.user.lastname}</div>
-    <div>Login Name:</div> <div>{this.props.user.username}</div> 
-    <div>First Name:</div> <div>{this.props.user.firstname}</div>
-    <div>Last Name:</div>  <div>{this.props.user.lastname}</div> 
-    <div>Email:</div>  <div>{this.props.user.email}</div> 
-    <div>Office:</div>  <div>{this.props.user.officeName}</div>
-    <div>Roles:</div> <div className={style.box_role}>{this.props.user.selectedRoles&& this.props.user.selectedRoles.map((value,i)=><p key={i}>{value.name}</p>)}</div> 
+    <div><Icon type="user" /> Login Name:</div> <div>{this.props.user.username}</div> 
+    <div><Icon type="user" /> First Name:</div> <div>{this.props.user.firstname}</div>
+    <div><Icon type="user" /> Last Name:</div>  <div>{this.props.user.lastname}</div> 
+    <div><Icon type="mail" /> Email:</div>  <div>{this.props.user.email}</div> 
+    <div><Icon type="team" /> Office:</div>  <div>{this.props.user.officeName}</div>
+    <div><Icon type="solution" /> Roles:</div> <div className={style.box_role}>{this.props.user.selectedRoles&& this.props.user.selectedRoles.map((value,i)=><p key={i}>{value.name}</p>)}</div> 
               </div>
               <div className="box-right">
                   {/* <Button type="primary" onClick={()=>{this.props.history.push('../edit/'+this.props.match.params.userId)}}>Edit</Button>
