89b7eaf60043916bf714e5357b3ea6c26d165a4e
dinhthuan <dinhthuanvan@gmail.com>
2020-02-18 15:52:14 +0700

    update code

--- src/views/user-management/userForm/userForm.js
+++ src/views/user-management/userForm/userForm.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "dva";
 import Layout from "../../../layouts/proLayout/mainProlayout";
-import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete, } from 'antd';
-
-const { Option } = Select;
-const AutoCompleteOption = AutoComplete.Option;
+import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete, message } from 'antd';
+import style from "./userForm.scss";
 
 class RegistrationForm extends React.Component {
    state = {
@@ -12,15 +10,25 @@ class RegistrationForm extends React.Component {
       autoCompleteResult: [],
    };
 
+   success = () => {
+      message.success('Add account successfully', 2);
+   };
+
+   error = () => {
+      message.error('Add account failed', 2);
+   };
+
+
    handleSubmit = e => {
       e.preventDefault();
       this.props.form.validateFieldsAndScroll((err, values) => {
          if (!err) {
-            console.log('Received values of form: ', values);
-            this.props.dispatch({
-               type: "users/addUser",
-               payload: values,
-            });
+            // console.log('Received values of form: ', values);
+            // this.props.dispatch({
+            //    type: "users/addUser",
+            //    payload: values,
+            // });
+            this.success();
          }
       });
    };
@@ -85,87 +93,109 @@ class RegistrationForm extends React.Component {
       };
 
       return (
-         <Form {...formItemLayout} onSubmit={this.handleSubmit} autoComplete="off">
-            <Form.Item label="Username">
-               {getFieldDecorator('username', {
-                  rules: [
-                     {
-                        required: true,
-                        message: 'Please input Username',
-                     },
-                  ],
-               })(<Input prefix={
-                  <Icon
-                     type="user"
-                     style={{ color: "rgba(0,0,0,.25)" }}
-                  />
-               }/>)}
-            </Form.Item>
-            <Form.Item label="Password" hasFeedback>
-               {getFieldDecorator('password', {
-                  rules: [
-                     {
-                        required: true,
-                        message: 'Please input your password!',
-                     },
-                     {
-                        validator: this.validateToNextPassword,
-                     },
-                  ],
-               })(<Input.Password />)}
-            </Form.Item>
-            <Form.Item label="Confirm Password" hasFeedback>
-               {getFieldDecorator('confirm', {
-                  rules: [
-                     {
-                        required: true,
-                        message: 'Please confirm your password!',
-                     },
-                     {
-                        validator: this.compareToFirstPassword,
-                     },
-                  ],
-               })(<Input.Password onBlur={this.handleConfirmBlur} />)}
-            </Form.Item>
-            <Form.Item
-               label={<span> mobileNo </span>}
-            >
-               {getFieldDecorator('mobileNo', {
-                  rules: [{ required: true, message: 'Please input your mobileNo!', whitespace: true }],
-               })(<Input />)}
-            </Form.Item>
+         <div className={style.boxForm}>
+            <Form {...formItemLayout} onSubmit={this.handleSubmit} autoComplete="off">
+               <Form.Item label="Username">
+                  {getFieldDecorator('username', {
+                     rules: [
+                        {
+                           required: true,
+                           message: 'Please input Username',
+                        },
+                     ],
+                  })(<Input prefix={
+                     <Icon
+                        type="user"
+                        style={{ color: "rgba(0,0,0,.25)" }}
+                     />
+                  } />)}
+               </Form.Item>
+               <Form.Item label="Password" hasFeedback>
+                  {getFieldDecorator('password', {
+                     rules: [
+                        {
+                           required: true,
+                           message: 'Please input your password!',
+                        },
+                        {
+                           validator: this.validateToNextPassword,
+                        },
+                     ],
+                  })(<Input.Password
+                     prefix={
+                        <Icon
+                           type="lock"
+                           style={{ color: "rgba(0,0,0,.25)" }}
+                        />
+                     } />)}
+               </Form.Item>
+               <Form.Item label="Confirm Password" hasFeedback>
+                  {getFieldDecorator('confirm', {
+                     rules: [
+                        {
+                           required: true,
+                           message: 'Please confirm your password!',
+                        },
+                        {
+                           validator: this.compareToFirstPassword,
+                        },
+                     ],
+                  })(<Input.Password
+                     prefix={
+                        <Icon
+                           type="lock"
+                           style={{ color: "rgba(0,0,0,.25)" }}
+                        />
+                     }
+                     onBlur={this.handleConfirmBlur} />)}
+               </Form.Item>
+               <Form.Item
+                  label={<span> mobileNo </span>}
+               >
+                  {getFieldDecorator('mobileNo', {
+                     rules: [{ required: true, message: 'Please input your mobileNo!', whitespace: true }],
+                  })(<Input
+                     prefix={
+                        <Icon
+                           type="phone"
+                           style={{ color: "rgba(0,0,0,.25)" }}
+                        />
+                     }
+                  />)}
+               </Form.Item>
 
-            <Form.Item
-               label={<span> jobNum </span>}
-            >
-               {getFieldDecorator('jobNum', {
-                  rules: [{ required: true, message: 'Please input your jobNum!', whitespace: true }],
-               })(<Input />)}
-            </Form.Item>
+               <Form.Item
+                  label={<span> jobNum </span>}
+               >
+                  {getFieldDecorator('jobNum', {
+                     rules: [{ required: true, message: 'Please input your jobNum!', whitespace: true }],
+                  })(<Input />)}
+               </Form.Item>
 
-            <Form.Item
-               label={<span> loginFlag </span>}
-            >
-               {getFieldDecorator('loginFlag', {
-                  rules: [{ required: true, message: 'Please input your loginFlag!', whitespace: true }],
-               })(<Input />)}
-            </Form.Item>
+               <Form.Item
+                  label={<span> loginFlag </span>}
+               >
+                  {getFieldDecorator('loginFlag', {
+                     rules: [{ required: true, message: 'Please input your loginFlag!', whitespace: true }],
+                  })(<Input />)}
+               </Form.Item>
 
-            <Form.Item
-               label={<span> acctStatus </span>}
-            >
-               {getFieldDecorator('acctStatus', {
-                  rules: [{ required: true, message: 'Please input your acctStatus!', whitespace: true }],
-               })(<Input />)}
-            </Form.Item>
+               <Form.Item
+                  label={<span> acctStatus </span>}
+               >
+                  {getFieldDecorator('acctStatus', {
+                     rules: [{ required: true, message: 'Please input your acctStatus!', whitespace: true }],
+                  })(<Input />)}
+               </Form.Item>
 
 
-            <Form.Item {...tailFormItemLayout}>
-               <Button type="primary" htmlType="submit">
-                  Register
+               <Form.Item {...tailFormItemLayout}>
+                  <Button type="primary" htmlType="submit">
+                     Register
            </Button>
-            </Form.Item>
-         </Form>
+               </Form.Item>
+            </Form>
+         </div>
       );
    }
 }
